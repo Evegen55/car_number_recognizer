1b62456461c89725acc85e237dfa978b1dcace2b
Evgenii Lartcev <i.dolende@gmail.com>
2018-11-10 19:24:58 +0300

    some improvements

--- src/test/java/digit/recogniser/nn/NeuralNetworkTest.java
+++ src/test/java/digit/recogniser/nn/NeuralNetworkTest.java
@@ -1,15 +1,13 @@
 package digit.recogniser.nn;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static digit.recogniser.data.IdxReader.VECTOR_DIMENSION;
 import static digit.recogniser.nn.NeuralNetwork.NEURAL_OUTPUT_CLASSES;
-import static digit.recogniser.util.Util.setHadoopHomeEnvironmentVariable;
 
 /**
- * @author (created on 12/7/2017).
+ * @author (created on 12 / 7 / 2017).
  */
 public class NeuralNetworkTest {
 
@@ -18,42 +16,29 @@ public class NeuralNetworkTest {
     private static final int[] LAYERS_BIGGER = new int[]{VECTOR_DIMENSION, 1024, 512, 128, 64, NEURAL_OUTPUT_CLASSES};
 
     @Before
-    public void setUp() throws Exception {
-        setHadoopHomeEnvironmentVariable();
+    public void setUp() {
+//        setHadoopHomeEnvironmentVariable(); //For Windows only
         NEURAL_NETWORK = new NeuralNetwork();
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
-    public void init() throws Exception {
-    }
-
-    @Test
-    public void train_1000d_1000t_withcustomlayers() throws Exception {
+    public void train_1000d_1000t_withcustomlayers() {
         NEURAL_NETWORK.train(1000, 1000, true, LAYERS_BIGGER);
     }
 
     @Test
-    public void train_30000_10000() throws Exception {
+    public void train_30000_10000() {
         NEURAL_NETWORK.train(30000, 10000, true, null);
     }
 
     @Test
-    public void train_60000_10000() throws Exception {
+    public void train_60000_10000() {
         NEURAL_NETWORK.train(60000, 10000, true, null);
     }
 
     @Test
-    public void train_60000_10000_withcustomlayers() throws Exception {
+    public void train_60000_10000_withcustomlayers() {
         NEURAL_NETWORK.train(60000, 10000, true, LAYERS_BIGGER);
-    }
-
-
-    @Test
-    public void predict() throws Exception {
     }
 
 }
